35b77abcc2746fdb95c5261c7f4d141215fdde01
Michel Ribeiro <michel.ribeiro1@hotmail.com>
2017-09-01 13:59:44 -0300

    Tela de cadastro adicionada

--- src/views/user.js
+++ src/views/user.js
@@ -4,3 +4,7 @@ require('../api/validarLogin.js');
 app.get('/login', function(req, res){
 	res.render('index.html', {username: req.session.username, name: req.session.name});
 });
+
+app.get('/cadastro', function(req, res){
+	res.render('cadastro.html');
+});
